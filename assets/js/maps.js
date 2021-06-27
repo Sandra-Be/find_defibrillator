26d2be98a14970617ed0864fa54b02c4f34fc5ec
Sandra-Be <sandraberzk@gmail.com>
2021-06-27 22:27:56 +0000

    edited lat and lng on added markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,3 +1,5 @@
+let map;
+
 //Google Maps  
 
 function initMap() {
@@ -12,105 +14,77 @@ function initMap() {
 // Array of markers
 var markers = [
   {
-    coords:{lat:52.1421626700037,lng:-9.305669412496405},
-    content:'<h1>AED Automatic External Defibrillator Ireland</h1>'
+    coords:{lat:52.10964436742095,lng:-8.689611793384062},
+    content:'<h1>1</h1>'
   },
   {
-    coords:{lat:52.256635299495386,lng:-6.866704684608732},
-    content:'<h1>Automated External Defibrillator</h1>'
+    coords:{lat:52.16124767975052,lng:-7.151566301940848},
+    content:'<h1>2</h1>'
   },
   {
-    coords:{lat:52.73154489314362,lng:-9.052983877625143},
-    content:'<h1>AED</h1>'
+    coords:{lat:52.62342012846225,lng:-8.811364243252473},
+    content:'<h1>3</h1>'
   },
   {
-    coords:{lat:52.77144327068455,lng:-8.26196829020212},
-    content:'<h1>AED Automated External Defibrillator</h1>'
+    coords:{lat:52.600774124237496,lng:-8.973489527752989},
+    content:'<h1>4</h1>'
   },
   {
-    coords:{lat:53.54217754684779,lng:-7.174321857495463},
-    content:'<h1>AED Automated External Defibrillator</h1>'
+    coords:{lat:53.38987638335143,lng:-6.2984262865546325},
+    content:'<h1>5</h1>'
   },
   {
-    coords:{lat:53.64650336613575,lng:-5.75708559669588},
-    content:'<h1>AED External Defibrillator</h1>'
+    coords:{lat:52.090375183987405,lng:-7.6237825080413115},
+    content:'<h1>7</h1>'
   },
   {
-    coords:{lat:52.11004406649266,lng:-7.630299956684575},
-    content:'<h1>AED External Defibrillator</h1>'
+    coords:{lat:52.361055475941946,lng:-9.059641701933769},
+    content:'<h1>9</h1>'
   },
   {
-    coords:{lat:52.10582685530924,lng:-7.554768954413278},
-    content:'<h1>AED External Defibrillator</h1>'
+    coords:{lat:52.45565387980471,lng:-8.927184873094818},
+    content:'<h1>10</h1>'
   },
   {
-    coords:{lat:52.37744001849238,lng:-9.017323852065012},
-    content:'<h1>AED</h1>'
+    coords:{lat:52.53418752737864,lng:-8.872767901927586},
+    content:'<h1>13</h1>'
   },
   {
-    coords:{lat:52.44850680974158,lng:-8.92571324415733},
-    content:'<h1>AED</h1>'
+    coords:{lat:52.589955964091324,lng:-6.494968747114094},
+    content:'<h1>15</h1>'
   },
   {
-    coords:{lat:52.48273892976059,lng:-8.939568417608076},
-    content:'<h1>AED</h1>'
+    coords:{lat:52.592775345058676,lng:-6.511480468536884},
+    content:'<h1>16</h1>'
   },
   {
-    coords:{lat:52.487274992935276,lng:-8.8276770066944},
-    content:'<h1>AED</h1>'
+    coords:{lat:52.622803895603894,lng:-6.47973401211503},
+    content:'<h1>17</h1>'
   },
   {
-    coords:{lat:52.549948926131606,lng:-8.834543461446335},
-    content:'<h1>AED</h1>'
+    coords:{lat:53.451105347975584,lng:-6.8445500706460525},
+    content:'<h1>19</h1>'
   },
   {
-    coords:{lat:52.623372384872134,lng:-8.977365720286604},
-    content:'<h1>AED</h1>'
+    coords:{lat:53.41418742468659,lng:-6.832294470765572},
+    content:'<h1>20</h1>'
   },
   {
-    coords:{lat:52.63893448431766,lng:-8.807104895539165},
-    content:'<h1>AED</h1>'
+    coords:{lat:53.44118991638063,lng:-6.806201942708192},
+    content:'<h1>21</h1>'
   },
   {
-    coords:{lat:52.6651775166355,lng:-6.520575380161987},
-    content:'<h1>AED Defibrillator</h1>'
+    coords:{lat:53.397711077135796,lng:-8.993340301896556},
+    content:'<h1>23</h1>'
   },
   {
-    coords:{lat:52.66101300727781,lng:-6.482123233551145},
-    content:'<h1>AED Defibrillator</h1>'
+    coords:{lat:54.120143380150935,lng:-6.738622673034752},
+    content:'<h1>24</h1>'
   },
   {
-    coords:{lat:53.28372713805419,lng:-7.025946443902594},
-    content:'<h1>AED</h1>'
-  },
-  {
-    coords:{lat:53.466942604162675,lng:-6.845950183685315},
-    content:'<h1>AED Public Access Defibrillator</h1>'
-  },
-  {
-    coords:{lat:53.461539377842044,lng:-6.737176859794313},
-    content:'<h1>AED Public Access Defibrillator</h1>'
-  },
-  {
-    coords:{lat:53.4018415316852,lng:-6.847386153626036},
-    content:'<h1>AED Public Access Defibrillator</h1>'
-  },
-  {
-    coords:{lat:53.49726015880319,lng:-7.868050964595465},
-    content:'<h1>Defibrillator</h1>'
-  },
-  {
-    coords:{lat:53.47798751919546,lng:-8.647153261420733},
-    content:'<h1>AED (Defibrillator) Machine - Corrandulla Community Hall</h1>'
-  },
-  {
-    coords:{lat:54.15630265306495,lng:-6.755046870316596},
-    content:'<h1>AED Public Access Defibrillator</h1>'
-  },
-  {
-    coords:{lat:55.104596298305786,lng:-7.633953078564402},
-    content:'<h1>AED - Public Access Defibrillator</h1>'
-  },
+    coords:{lat:55.03660301953727,lng:-7.6509687018365735},
+    content:'<h3>25</h3>'
+  }
 ];
 
 // Loop through markers

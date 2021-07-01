e3de19ad5fb2d73ad194e4073ef094197e43e5e1
Sandra-Be <sandraberzk@gmail.com>
2021-07-01 01:40:11 +0000

    marker locations moved from maps.js to markers.js

---
+++ assets/js/markers.js
@@ -0,0 +1,245 @@
+const CORK = [
+    {
+        coords: { lat: 52.10964436742095, lng: -8.689611793384062 },
+        content:
+            '<div class="icon-content"><p>Newberry Cl</p><br><p>Dromore</p><br><p>Mallow</p><br><p>County Cork</p></div>',
+        
+    },
+];
+
+const WATERFORD = [
+    {
+        coords: { lat: 52.16124767975052, lng: -7.151566301940848 },
+        content:
+            '<div class="icon-content"><p>Tramore Post Office</p><br><p>1 Broad St</p><br><p>Tramore West</p><br><p>Tramore</p><br><p>County Waterford</p><br><p>X91 XN36</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.090375183987405, lng: -7.6237825080413115 },
+        content:
+            '<div class="icon-content"><p>Dungarvan Shopping Centre</p><br><p>Shandon St</p><br><p>Dungarvan</p><br><p>County Waterford</p></div>',
+        
+    },
+];
+
+const LIMERICK = [
+    {
+        coords: { lat: 52.62342012846225, lng: -8.811364243252473 },
+        content:
+            '<div class="icon-content"><p>Ballyvareen</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.600774124237496, lng: -8.973489527752989 },
+        content:
+            '<div class="icon-content"><p>Askeaton</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.361055475941946, lng: -9.059641701933769 },
+        content:
+            '<div class="icon-content"><p>Clooncon</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.45565387980471, lng: -8.927184873094818 },
+        content:
+            '<div class="icon-content"><p>Cloncagh</p><br><p>Clounagh</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.53418752737864, lng: -8.872767901927586 },
+        content:
+            '<div class="icon-content"><p>Smithfield</p><br><p>Croagh</p><br><p>County Limerick</p></div>',
+        
+    },
+];
+
+const DUBLIN = [
+    {
+        coords: { lat: 53.38987638335143, lng: -6.2984262865546325 },
+        content:
+            '<div class="icon-content"><p>Grafton Hall</p><br><p>Jamestown Rd</p><br><p>Ballygall</p><br><p>Dublin 11</p></div>',
+        
+    },
+];
+
+const WEXFORD = [
+    {
+        coords: { lat: 52.589955964091324, lng: -6.494968747114094 },
+        content:
+            '<div class="icon-content"><p>Applegreen</p><br><p>Ferns Lower</p><br><p>County Wexford</p><br><p>Y21 C6F7</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.592775345058676, lng: -6.511480468536884 },
+        content:
+            '<div class="icon-content"><p>Ferns Upper</p><br><p>County Wexford</p></div>',
+       
+    },
+    {
+        coords: { lat: 52.622803895603894, lng: -6.47973401211503 },
+        content:
+            '<div class="icon-content"><p>Clonee Lower</p><br><p>County Wexford</p><br><p>Y21 F228</p></div>',
+        
+    },
+];
+
+const MEATH = [
+    {
+        coords: { lat: 53.451105347975584, lng: -6.8445500706460525 },
+        content:
+            '<div class="icon-content"><p>Unnamed Road</p><br><p>Rathcore</p><br><p>County Meath</p></div>',
+        
+    },
+    {
+        coords: { lat: 53.41418742468659, lng: -6.832294470765572 },
+        content:
+            '<div class="icon-content"><p>Dublin Rd</p><br><p>Johnstown</p><br><p>County Meath</p></div>',
+        
+    },
+    {
+        coords: { lat: 53.44118991638063, lng: -6.806201942708192 },
+        content:
+            '<div class="icon-content"><p>R159</p><br><p>County Meath</p></div>',
+        
+    },
+];
+
+const GALWAY = [
+    {
+        coords: { lat: 53.397711077135796, lng: -8.993340301896556 },
+        content:
+            '<div class="icon-content"><p>Corrandulla Community Hall</p><br><p>Ballybeg</p><br><p>County Galway</p></div>',
+        
+    },
+];
+
+const MONAGHAN = [
+    {
+        coords: { lat: 54.120143380150935, lng: -6.738622673034752 },
+        content:
+            '<div class="icon-content"><p>Main St</p><br><p>Conabury</p><br><p>Castleblayney</p><br><p>County Monaghan</p></div>',
+        
+    },
+];
+
+const DONEGAL = [
+    {
+        coords: { lat: 55.03660301953727, lng: -7.6509687018365735 },
+        content:
+            '<div class="icon-content"><p>Whoriskey Eurospar</p><br><p>The Mall</p><br><p>Ballyboe</p><br><p>Ramelton</p><br><p>County Donegal</p><br><p>F92 TF6A</p></div>',
+        
+    },
+];
+
+const IRELAND = [
+    {
+        coords: { lat: 55.03660301953727, lng: -7.6509687018365735 },
+        content:
+            '<div class="icon-content"><p>Whoriskey Eurospar</p><br><p>The Mall</p><br><p>Ballyboe</p><br><p>Ramelton</p><br><p>County Donegal</p><br><p>F92 TF6A</p></div>',
+        
+    },
+    {
+        coords: { lat: 54.120143380150935, lng: -6.738622673034752 },
+        content:
+            '<div class="icon-content"><p>Main St</p><br><p>Conabury</p><br><p>Castleblayney</p><br><p>County Monaghan</p></div>',
+        
+    },
+    {
+        coords: { lat: 53.397711077135796, lng: -8.993340301896556 },
+        content:
+            '<div class="icon-content"><p>Corrandulla Community Hall</p><br><p>Ballybeg</p><br><p>County Galway</p></div>',
+        
+    },
+    {
+        coords: { lat: 53.451105347975584, lng: -6.8445500706460525 },
+        content:
+            '<div class="icon-content"><p>Unnamed Road</p><br><p>Rathcore</p><br><p>County Meath</p></div>',
+        
+    },
+    {
+        coords: { lat: 53.41418742468659, lng: -6.832294470765572 },
+        content:
+            '<div class="icon-content"><p>Dublin Rd</p><br><p>Johnstown</p><br><p>County Meath</p></div>',
+        
+    },
+    {
+        coords: { lat: 53.44118991638063, lng: -6.806201942708192 },
+        content:
+            '<div class="icon-content"><p>R159</p><br><p>County Meath</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.589955964091324, lng: -6.494968747114094 },
+        content:
+            '<div class="icon-content"><p>Applegreen</p><br><p>Ferns Lower</p><br><p>County Wexford</p><br><p>Y21 C6F7</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.592775345058676, lng: -6.511480468536884 },
+        content:
+            '<div class="icon-content"><p>Ferns Upper</p><br><p>County Wexford</p></div>',
+       
+    },
+    {
+        coords: { lat: 52.622803895603894, lng: -6.47973401211503 },
+        content:
+            '<div class="icon-content"><p>Clonee Lower</p><br><p>County Wexford</p><br><p>Y21 F228</p></div>',
+        
+    },
+    {
+        coords: { lat: 53.38987638335143, lng: -6.2984262865546325 },
+        content:
+            '<div class="icon-content"><p>Grafton Hall</p><br><p>Jamestown Rd</p><br><p>Ballygall</p><br><p>Dublin 11</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.62342012846225, lng: -8.811364243252473 },
+        content:
+            '<div class="icon-content"><p>Ballyvareen</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.600774124237496, lng: -8.973489527752989 },
+        content:
+            '<div class="icon-content"><p>Askeaton</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.361055475941946, lng: -9.059641701933769 },
+        content:
+            '<div class="icon-content"><p>Clooncon</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.45565387980471, lng: -8.927184873094818 },
+        content:
+            '<div class="icon-content"><p>Cloncagh</p><br><p>Clounagh</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.53418752737864, lng: -8.872767901927586 },
+        content:
+            '<div class="icon-content"><p>Smithfield</p><br><p>Croagh</p><br><p>County Limerick</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.16124767975052, lng: -7.151566301940848 },
+        content:
+            '<div class="icon-content"><p>Tramore Post Office</p><br><p>1 Broad St</p><br><p>Tramore West</p><br><p>Tramore</p><br><p>County Waterford</p><br><p>X91 XN36</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.090375183987405, lng: -7.6237825080413115 },
+        content:
+            '<div class="icon-content"><p>Dungarvan Shopping Centre</p><br><p>Shandon St</p><br><p>Dungarvan</p><br><p>County Waterford</p></div>',
+        
+    },
+    {
+        coords: { lat: 52.10964436742095, lng: -8.689611793384062 },
+        content:
+            '<div class="icon-content"><p>Newberry Cl</p><br><p>Dromore</p><br><p>Mallow</p><br><p>County Cork</p></div>',
+        
+    },
+];

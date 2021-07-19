4eab88cd7e4e8b12c5ecbf57598d5a2f3c4ae99b
Sandra-Be <sandraberzk@gmail.com>
2021-07-19 20:49:45 +0000

    removed toggle functions and replaced with collapse Navbar on mobile device on click function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,4 @@
-// create variables for Navigation bar
-
-var navbar = document.getElementById('navbar');
-var toggle = document.getElementById('toggle');
-var menu = document.getElementById('menu');
-
-// click function for click on outside navigtion toggle
-
-    document.onclick = function(x){
-        if(x.target.id !== 'navbar' && x.target.id !== 'toggle' && x.target.id !== 'menu'){
-            toggle.classList.remove('active');
-            menu.classList.remove('active');
-        }
-    }
-
-// on click function for toggle in navigatin bar
-
-    toggle.onclick = function(){
-        toggle.classList.toggle('active');
-        menu.classList.toggle('active');
-    }
+// collapse Navbar on mobile device on Click
+$(".navbar-nav>li>a").click(function () {
+    $(".navbar-collapse").collapse("hide");
+});
